0058f0643efa474f33b08daafb05d9193a13e840
hp <mohamedoraby0500@gmail.com>
2025-09-06 14:50:00 +0300

    hive init

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,3 +5,4 @@ const kBackgroundColor = Color(0xFF212832);
 const kMainColor = Color(0xFFFED36A);
 const kLabelTextColor = Color(0xFF8CAAB9);
 const kFillTextFormColor = Color(0xFF455A64);
+const kTaskBox = 'taskBox';
--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,19 @@ import 'package:day_task/constants.dart';
 import 'package:day_task/provider/user_provider.dart';
 import 'package:day_task/utilitis/app_routes.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kTaskBox);
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(ChangeNotifierProvider(
-    create: (_) => UserProvider(),
-    child: const MyApp()));
+  runApp(
+    ChangeNotifierProvider(create: (_) => UserProvider(), child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
