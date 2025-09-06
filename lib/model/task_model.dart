55700a495df9464e2ff67f4a638e84127cc3bda7
hp <mohamedoraby0500@gmail.com>
2025-09-06 14:45:39 +0300

    generate hive type adapter

--- lib/model/task_model.dart
+++ lib/model/task_model.dart
@@ -1,8 +1,17 @@
-class TaskModel {
+import 'package:hive/hive.dart';
+part 'task_model.g.dart';
+
+@HiveType(typeId: 0)
+class TaskModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String details;
+  @HiveField(2)
   final String time;
+  @HiveField(3)
   final String date;
+  @HiveField(4)
   final List<String> teamMembers;
 
   TaskModel({required this.title, required this.details, required this.time, required this.date, required this.teamMembers});
---
+++ lib/model/task_model.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TaskModelAdapter extends TypeAdapter<TaskModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  TaskModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TaskModel(
+      title: fields[0] as String,
+      details: fields[1] as String,
+      time: fields[2] as String,
+      date: fields[3] as String,
+      teamMembers: (fields[4] as List).cast<String>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TaskModel obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.details)
+      ..writeByte(2)
+      ..write(obj.time)
+      ..writeByte(3)
+      ..write(obj.date)
+      ..writeByte(4)
+      ..write(obj.teamMembers);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
