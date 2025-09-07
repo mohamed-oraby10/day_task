6fa4cedc9c9af0402ffcae8b203ff021b3e37233
hp <mohamedoraby0500@gmail.com>
2025-09-07 15:23:27 +0300

    create addTask method

--- lib/cubits/add task cubit/add_task_cubit.dart
+++ lib/cubits/add task cubit/add_task_cubit.dart
@@ -1,6 +1,20 @@
+import 'package:day_task/constants.dart';
 import 'package:day_task/cubits/add%20task%20cubit/add_task_state.dart';
+import 'package:day_task/model/task_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 
-class AddTaskCubit extends Cubit<AddTaskState>{
+class AddTaskCubit extends Cubit<AddTaskState> {
   AddTaskCubit() : super(AddTaskInitial());
+
+  addTask(TaskModel task) async {
+    emit(AddTaskLoading());
+    try {
+      var tasksBox = Hive.box<TaskModel>(kTaskBox);
+      emit(AddTaskSuccess());
+      await tasksBox.add(task);
+    } catch (e) {
+      AddTaskFailure(e.toString());
+    }
+  }
 }
